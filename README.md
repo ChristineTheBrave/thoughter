7304a69f00daa31d5416c7cfbbd07cbd60feed3e
Christine Ash <ChristineTheBrave@Christines-MacBook-Pro.local>
2017-03-17 15:34:10 -0400

    Updated README.md with info on how to create a new build. Added tasks to gruntfile for karma, sass and jquery. Added jquery to html file. Added semicolon to new thought js file. Removed item from main scss file

--- README.md
+++ README.md
@@ -4,4 +4,14 @@
 Definitely not a Twitter clone.
 This is an assignment for my Front End class at The Iron Yard.
 This was to practice writing asynchronous tests and ones that require a specific piece of HTML to execute.
+Added a build process.
+In order to create a new build:
+1. Run npm install --save-dev grunt in your terminal
+2. Create a gruntfile.js in your root directory
+3. Run npm install grunt-contrib-copy --save-dev
+4. In gruntfile, write your first task (which should be copy)
+5. Ensure you can run grunt build from the command line after doing so
+6. Install the rest of your dependencies and their correlating tasks
+7. Throughout the process, continuously run grunt built to ensure your tasks run to completion
+Included folder for sass and js.
 The author of this project is Christine Ash.
---
+++ gruntfile.js
@@ -0,0 +1,74 @@
+
+module.exports = function configureGrunt(gruntConfig) {
+
+    gruntConfig.initConfig({
+
+      clean: ['build/'],
+
+      copy:{
+        copythehtml: {
+          files: [
+            {
+              cwd: 'src/',
+              src: [ '*.html' ],
+              dest: 'build/',
+              expand: true
+            }
+          ]
+        },
+
+
+        copythejquery: {
+          files: [
+            {
+              cwd: 'src/js',
+              src: ['jquery.js'],
+              dest: 'build/js/vendor/',
+              expand: true
+            }
+          ]
+        }
+      },
+      sass: {
+        all: {
+          files: {
+            'build/style.css': 'src/sass/main.scss'
+          }
+        }
+      },
+
+      jshint: {
+        appjs: {
+          options: {
+            jshintrc: '.jshintrc'
+          },
+          files: {
+            src: ['src/**/*.js']
+          }
+        }
+      },
+
+      karma:{
+        all: {
+          options: {
+            frameworks: ['mocha', 'chai'],
+            browsers: ['Chrome'],
+            singleRun: true,
+            files: [
+              //globbing pattern
+              //look in src/ directory and ANY subdirectory
+              //for ANY file that ends in .js
+              'src/**/*.js',
+              'node_modules/sinon/pkg/sinon-2.0.0.js',
+              'test/specs/**/*.js'
+            ]
+          }
+        }
+      }
+    });
+
+    require('load-grunt-tasks')(gruntConfig);
+
+    gruntConfig.registerTask('build',['jshint','karma','clean','copy','sass']);
+
+};
